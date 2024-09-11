e319e19cb0009bb73fc33ffc5326a877aa2ae9c1
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2024-09-10 23:55:51 -0300

    build v1 - petshop api

---
+++ src/main/java/me/dio/spring_pet_api/SpringPetApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.spring_pet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetApiApplication.class, args);
+	}
+
+}
