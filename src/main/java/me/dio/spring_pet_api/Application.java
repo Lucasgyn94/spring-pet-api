9c7f2d0416852c52a2ac36d5ebdcc198f622f47f
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2024-09-26 20:38:18 -0300

    fix bug cors

--- src/main/java/me/dio/spring_pet_api/Application.java
+++ src/main/java/me/dio/spring_pet_api/Application.java
@@ -2,11 +2,12 @@ package me.dio.spring_pet_api;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Pet Shop API", version = "1.0", description = "API para gerenciamento de um Pet Shop"))
+@OpenAPIDefinition(servers = {@Server(url = "/")}, info = @Info(title = "Pet Shop API", version = "1.0", description = "API para gerenciamento de um Pet Shop"))
 public class Application {
 
 	public static void main(String[] args) {
