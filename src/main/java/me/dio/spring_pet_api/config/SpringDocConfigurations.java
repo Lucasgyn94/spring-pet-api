75ff0f0b0d3da6167c66e0dfe48d17fc6b41d465
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2024-09-26 21:42:29 -0300

    fix bug authorization bearer

--- src/main/java/me/dio/spring_pet_api/config/SpringDocConfigurations.java
+++ src/main/java/me/dio/spring_pet_api/config/SpringDocConfigurations.java
@@ -1,8 +1,8 @@
 package me.dio.spring_pet_api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +13,12 @@ public class SpringDocConfigurations {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .info(new Info()
                         .title("Pet Shop API")
                         .version("1.0")
-                        .description("API para gerenciamento de um Pet Shop")
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .schemaRequirement("bearerAuth", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT"));
+                        .description("API para gerenciamento de um Pet Shop"));
     }
 }
